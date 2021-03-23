63a87e4effa0e75e1d74226e7c57850772c97d3e
shoiti <shoiti.tokunaga@gmail.com>
2021-03-23 13:46:57 -0300

    React Native - style e StyleSheet

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,54 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 const Header = function() {
     return (
-      <View>
-        <Text>Titulo</Text>
+      <View style={style.container}>
+        <Text style={style.text}>Titulo</Text>
       </View>
     )
 }
+
+const style = StyleSheet.create({
+    container: {
+        backgroundColor: 'blue'
+    },
+    text: {
+        fontSize: 24,
+        color: 'white',
+        margin: 20,
+        textAlign: 'center'
+    }
+})
+
+/*
+StyleSheet.create serve como otimização, ele te ajuda a saber onde vc está errado
+
+Alternativa 1 - É possivel passar um objeto direto no style sem o StyleSheet.create
+const style = {
+    container: {
+        backgroundColor: 'blue'
+    },
+    text: {
+        fontSize: 24,
+        color: 'white',
+        margin: 20,
+        textAlign: 'center'
+    }
+}
+
+Alternativa 2 - Passado separado com ou sem StyleSheet.create
+const container: {
+    backgroundColor: 'blue'
+}
+const text: {
+    fontSize: 24,
+    color: 'white',
+    margin: 20,
+    textAlign: 'center'
+}
+
+
+*/
 
 export default Header;
