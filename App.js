e516b4c32df6a11b4d70748493234db9b5ae2f8f
shoiti <shoiti.tokunaga@gmail.com>
2021-03-23 13:37:13 -0300

    React Native  - Criação de um componente em arquivos separado

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './src/components/header'
+
 export default function App() {
   return (
     <View>
+      <Header/>
       <Text>Olá mundo!</Text>
     </View>
   );
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = function() {
+    return (
+      <View>
+        <Text>Titulo</Text>
+      </View>
+    )
+}
+
+export default Header;
