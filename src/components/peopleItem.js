c7a67ccaaab6e66496feeecc9587903675599b0f
shoiti <shoiti.tokunaga@gmail.com>
2021-03-25 12:30:07 -0300

    React Native - Adicionando component PeopleItem

---
+++ src/components/peopleItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+const PeopleItem = function(props) {
+    const pessoa = props.pessoa;
+    const first = pessoa.name.first
+    const last = pessoa.name.last
+
+    return (
+        <View key={first} style={style.item}>
+            <Image style={style.img} source={{ uri: pessoa.picture.thumbnail }}/>
+            <Text style={style.text}>{`${first} ${last}`}</Text> 
+        </View>
+    )
+} 
+const style = StyleSheet.create({
+    item: {
+        backgroundColor: '#96d7ff',
+        height: 50,
+        alignItems: 'center',
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderColor: 'gray'
+    },
+    img: {
+        aspectRatio: 1,
+        width: 10,
+        borderRadius: 50,
+        flex: 1,
+        margin: 5
+    },
+    text: {
+        marginLeft: 8,
+        flex: 8
+    }
+})
+
+export default PeopleItem;
--- src/components/peopleList.js
+++ src/components/peopleList.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+import PeopleItem from './peopleItem'
+
 const PeopleList = function(props) {
     const pessoas = props.pessoas;
     const pessoasText = pessoas.map(pessoa => {
-        const first = pessoa.name.first
         return (
-            <View key={first} style={style.item}>
-                <Text>{first}</Text>
-            </View>
+            <PeopleItem pessoa={pessoa}/>
         );
     })
     
