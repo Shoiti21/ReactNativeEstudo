e0466969acc30a9db0ff789f98277d3ea436b946
shoiti <shoiti.tokunaga@gmail.com>
2021-03-25 17:44:38 -0300

    React Native - react-navigation 5

--- App.js
+++ App.js
@@ -1,80 +1,21 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { StyleSheet, Text, View } from 'react-native';
-// Lib para fazer chamadas REST API
-import axios from 'axios';
+import * as React from 'react';
+import { View, Text } from 'react-native';
 
-import Header from './src/components/header'
-import PeopleList from './src/components/peopleList'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default class App extends React.Component {
+import PeoplePage from './src/pages/peoplePage'
 
-  constructor(props) {
-    super(props);
+const StackNavigator = createStackNavigator();
 
-    this.state = {
-      pessoas: []
-    };
-  }
-
-  // MEDOTO DO REACT - Ele é chamado pelo react quando é completado, portanto, chamado somente depois do render(). A chamada é feito uma vez.
-  componentDidMount()  {
-    // Chamada GET de uma REST no axios
-    axios
-      .get('https://randomuser.me/api/?results=10')
-      .then( (res) => {
-        const results = res.data.results; // const {results} = res.data;
-        console.log(results);
-        this.setState({
-          pessoas: results
-        });
-      })
-  }
-
-  // SOMENTE O RENDER() É RECONHECIDO PELO REACT, PORTANTO, REACT EXECUTA AUTOMATICO SOMENTE O METODO RENDER() 
-  render() {
-    return (
-      <View>
-        <Header title="Pessoas"/>
-        <PeopleList pessoas={this.state.pessoas}/>
-      </View>
-    );
-  }
-
-}
-
-/*
-Os três metodos tem o mesmo retorno
-
-Alternativa 1 - Functionanl
-const App = function() {
+function App() {
   return (
-    <View>
-      <Text>Olá mundo!</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <StackNavigator.Navigator>
+        <StackNavigator.Screen name="Pessoas" component={PeoplePage} />
+      </StackNavigator.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App;
 
-
-Alternativa 2 - Functional with Array Function 
-const App = () => (
-  <View>
-    <Text>Olá mundo!</Text>
-  </View>
-);
 export default App;
- 
-Alternativa  3 - Class
-class App extends React.Component {
-  render() {
-    return (
-      <View>
-        <Text>Olá mundo!</Text>
-      </View>
-    );
-  }
-}
-export default App;
-
-*/
