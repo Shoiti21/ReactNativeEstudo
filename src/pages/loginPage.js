5d226aedfffc2c94ff63db7902b0372aea2432c2
shoiti <shoiti.tokunaga@gmail.com>
2021-03-30 14:43:28 -0300

    React Native - Login com Auth do Firebase

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from 'firebase';
 import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 
 /*
@@ -29,24 +30,58 @@ export default class loginPage extends React.Component {
     }
 
     doLogin() {
-        if (this.state.login.length > 5) {
+        if (this.state.login.length > 5 && this.state.password.length > 5) {
             var regChar = /[\W]/;
-            if (regChar.test(this.state.login)) {
+            if (regChar.test(this.state.login) && regChar.test(this.state.password)) {
                 this.setState({
-                    message: "Usuário não pode ter caracter especial"
+                    message: "E-mail/Senha não pode ter caracter especial"
                 })
             } else {
-                console.log("Sucesso!")
                 this.setState({
                     message: "",
                     loading: true
                 })
+                this.doAuthFirebase()
             }
         } else {
             this.setState({
-                message: "Usuário não pode ser menos de 5 caracteres"
+                message: "E-mail/Senha não pode ser menos de 5 caracteres"
             })
         }
+    }
+
+    doAuthFirebase() {
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.login, this.state.password)
+            .then( result => {
+                console.log('Sucesso! ' + result);
+                this.setState({
+                    message: ""
+                })
+                this.props.navigation.navigate('Pessoas');
+            })
+            .catch( erro => {
+                console.log('Erro! ' + erro);
+                this.setState({
+                    message: "E-mail/Senha incorreto",
+                    loading: false
+                })
+            })
+    }
+
+    componentDidMount() {
+        var firebaseConfig = {
+            apiKey: "AIzaSyCns2xjkP7dG6tGilFJZCb7WGCQIwSoEYg",
+            authDomain: "reactnativeestudo.firebaseapp.com",
+            projectId: "reactnativeestudo",
+            storageBucket: "reactnativeestudo.appspot.com",
+            messagingSenderId: "1095379320498",
+            appId: "1:1095379320498:web:fb56e4b1728ffeec0753ab",
+            measurementId: "G-GQB3CGH5V5"
+          };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
     }
 
     render() {
